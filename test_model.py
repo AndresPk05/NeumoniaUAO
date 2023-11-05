ff720e09f2add823884fc06f8727893db9925f83
Andres parrado <parrado.andresb@gmail.com>
2023-11-04 19:47:48 -0500

    [Commit]:"Create new unittest call test_model to test the model with viral image"

--- test_model.py
+++ test_model.py
@@ -1,6 +1,19 @@
 import unittest
+from Image import ImageFile
+import PredictNeomony
 
-class TestModel(unittest.Testcase):
+class TestModel(unittest.TestCase):
     def setUp(self):
-        self.reportID = 5
-        self.root = Tk()
+        self.image = ImageFile()
+        self.filepath = "viral (3).dcm"
+
+    def test_predict_neomony(self):
+        array, img2show = self.image.read_dicom_file(self.filepath)
+        label, proba, heatmap = PredictNeomony.predict(array)
+        self.assertEqual(label, "viral")
+        self.assertEqual(proba, 88.81623148918152)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
